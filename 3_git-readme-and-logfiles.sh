84fde868264fec147c1a51f7ddc5f15e0436495b
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2024-12-09 14:56:12 +0800

    ✨ feat: enhance script to add input and output files
    - Add support for including input and output files in the git repository.
    - Introduce `inputFolder` and `outputFolder` variables for specifying input and output directories.
    - Add input files if the `inputFolder` exists and commit with the message "Add input file".
    - Check for the existence of log files and commit them only if they are present.
    - Add output files if the `outputFolder` exists and commit with the message "Add output (log) files".
    
    This update enhances the script's functionality by allowing it to handle input and output files, along with existing log and image files.

--- 3_git-readme-and-logfiles.sh
+++ 3_git-readme-and-logfiles.sh
@@ -4,6 +4,8 @@ readonly frontImageFile="front_view.png"
 readonly isometricImageFile="isometric_view.png"
 readonly xProjectionImageFile="left_view.png"
 readonly logFolder="log_files/"
+readonly inputFolder="Input file"
+readonly outputFolder="Output files"
 
 if [ -f "README.md" ]; then
     if [ -f "$frontImageFile" ]; then git add $frontImageFile; fi
@@ -11,10 +13,23 @@ if [ -f "README.md" ]; then
     git commit -m "Add README file"
 fi
 
-if ls log.* 1> /dev/null 2>&1; then git add log.* ; fi
-if ls -- *.log 1> /dev/null 2>&1; then git add -- *.log ; fi
-if ls $logFolder 1> /dev/null 2>&1; then git add $logFolder ; fi
-git commit -m "Add log files"
+if [ -d "$inputFolder" ]; then
+    git add "$inputFolder"
+    git commit -m "Add input file"
+fi
+
+hasLogfiles=false
+if ls log.* 1> /dev/null 2>&1; then git add log.* ; hasLogfiles=true ; fi
+if ls -- *.log 1> /dev/null 2>&1; then git add -- *.log ; hasLogfiles=true ; fi
+if ls $logFolder 1> /dev/null 2>&1; then git add $logFolder ; hasLogfiles=true ; fi
+if $hasLogfiles ; then
+    git commit -m "Add log files"
+fi
+
+if [ -d "$outputFolder" ]; then
+    git add "$outputFolder"
+    git commit -m "Add output (log) files"
+fi
 
 if [ -f "$isometricImageFile" ]; then
     git add $isometricImageFile

4974bd20e235cbc10a95e671193bfd5db2add719
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2024-12-09 14:54:14 +0800

    🔧 chore: disable GPG signing for commits in local repository
    - Disable GPG signing for commits in the local repository by setting `commit.gpgsign` to `false` in the `2_setup-git.sh` script.
    
    This update ensures that GPG signing is disabled for commits, which can help streamline the commit process in the docker environments.

--- 2_setup-git.sh
+++ 2_setup-git.sh
@@ -6,3 +6,6 @@ git init
 # Configure Git user information
 git config --local user.email "${GITHUB_EMAIL}"
 git config --local user.name "${GITHUB_USERNAME}"
+
+# Disable GPG signing for commits in the local repository
+git config --local commit.gpgsign false

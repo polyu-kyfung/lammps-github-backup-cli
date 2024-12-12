11f5e169a84188a20ca8236d3c671c39a1b56aff
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2024-12-12 23:31:43 +0800

    🔧 chore: rename constants for improved readability
    - Rename `branch` to `BRANCH` for the remote branch name.
    - Rename `delay` to `DELAY_MINUTES` for the time interval between pushing each commit.
    - Update variable references to use the new constant names.
    - Improve input validation messages.
    - Ensure consistent indentation and formatting.
    
    These changes enhance the script's readability and make it easier to maintain.

--- 7_progressive-push-latest-commits.sh
+++ 7_progressive-push-latest-commits.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
 
-branch="master" # Remote branch name
-delay="5" # Time interval between pushing each commit (in minutes)
+# Constants
+BRANCH="master"         # Remote branch name
+DELAY_MINUTES=3         # Time interval between pushing each commit (in minutes)
 
 # Get the number of unpushed commits
-count=$(git rev-list --count origin/${branch}..HEAD^)
-if [[ ! "$count" ]]; then
-	echo "Error: No commits could be found."
-	exit
+count=$(git rev-list --count origin/${BRANCH}..HEAD^)
+if [[ -z "$count" ]]; then
+    echo "Error: No commits could be found."
+    exit 1
 fi
 
+# Loop to prompt user for input until a valid integer is provided
 while true; do
-	# Prompt user to input an integer
-	echo "Enter the HEAD~ number [current: $count]:"
-	read -r num
-	num="${num:=$count}"
-	# Validate the input is an integer smaller than $count
-	if [[ "$num" =~ ^[0-9]+$ && "$num" -le "$count" ]]; then
-		break
-	else
-		echo "Invalid input. Please enter an integer smaller than ${count}."
-	fi
+    # Prompt user to input an integer
+    echo "Enter the HEAD~ number [current: $count]:"
+    read -r num
+    num="${num:=$count}"
+    # Validate that the input is an integer and less than or equal to $count
+    if [[ "$num" =~ ^[0-9]+$ && "$num" -le "$count" ]]; then
+        break
+    else
+        echo "Invalid input. Please enter an integer smaller than or equal to $count."
+    fi
 done
 
 # Push HEAD~$num to HEAD~1 one by one with a delay in between
-readonly last=1
-queue=$(seq "$num" -1 $last)
+readonly LAST=1
+queue=$(seq "$num" -1 $LAST)
 for i in $queue; do
-	echo "Pushing HEAD~${i} to remote:"
-	git show HEAD~${i} -s
-	
-	git push origin HEAD~${i}:${branch}
+    echo "Pushing HEAD~$i to remote:"
+    git show HEAD~$i -s
+    
+    git push origin HEAD~$i:$BRANCH
 
-	if (( i > last )); then
-			echo "Sleeping. Resume in ${delay} minutes."
-			sleep ${delay}m
-	fi
+    if (( i > LAST )); then
+        echo "Sleeping. Resume in $DELAY_MINUTES minutes."
+        sleep "${DELAY_MINUTES}m"
+    fi
 done
 
-# Push HEAD to remotecd
+# Push the latest commit (HEAD) to the remote repository
 echo "Pushing HEAD to remote:"
 git show HEAD -s
-git push origin HEAD:${branch}
+git push origin HEAD:$BRANCH

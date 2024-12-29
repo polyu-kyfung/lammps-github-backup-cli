bbcd7e6eed5f98ccb6416669d63485d8e31de9a9
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2024-12-29 19:06:34 +0800

    🔨 feat(script): add support for 'restart' file type in commit-result-files.sh
    - Enhanced user input options to include 'restart' for file type selection.
    - Updated file prefix and suffix handling for all file types.
    - Adjusted git add commands to reflect new file naming conventions.

--- 6_commit-result-files.sh
+++ 6_commit-result-files.sh
@@ -3,27 +3,33 @@
 # Prompt user to input the file type
 # Determine the glob patterns used to filter target files
 while true; do
-  echo "Enter file type [dump/force/rerun]:"
+  echo "Enter file type [dump/force/rerun/restart]:"
   read -r filetype
 
   case $filetype in
       dump)
           dirname="dumpfiles"  # Default directory name
           aliasdir="dump_files"  # Alias directory name
-          fileprefix="dump.cutsic"  # File name prefix
-          filesuffix="lammpstrj.gz*"  # File name suffix
+          fileprefix="dump.cutsic."  # File name prefix
+          filesuffix=".lammpstrj.gz*"  # File name suffix
           break;;
       force)
           dirname="force_files"
           aliasdir="dump*force"  # Support using the * wildcard
-          fileprefix="dump.force.cut_sic"
-          filesuffix="gz*"  # Support using the * wildcard
+          fileprefix="dump.force.cut_sic."
+          filesuffix=".gz*"  # Support using the * wildcard
           break;;
       rerun)
           dirname="rerun_files"
           aliasdir="dump_rerun"
-          fileprefix="dump.rerun.cut_sic"
-          filesuffix="gz*"  # Support using the * wildcard
+          fileprefix="dump.rerun.cut_sic."
+          filesuffix=".gz*"  # Support using the * wildcard
+          break;;
+      restart)
+          dirname="restart_files"
+          aliasdir="restart"
+          fileprefix="cut_sic.restart."
+          filesuffix=""  # Support using the * wildcard
           break;;
       *)
           echo "Invalid input"
@@ -49,12 +55,12 @@ fi
 
 # Create commits according to the user inputs
 # Determine the number of iterations required to commit all files based on cutting speed
-if [[ -n "$dirname" && -n "$fileprefix" && -n "$filesuffix" ]]; then
+if [[ -n "$dirname" && -n "$fileprefix" && ("$filetype" == "restart" || -n "$filesuffix" ) ]]; then
     
     echo "git add ${dirname}/*"
-    git add "${dirname}/${fileprefix}.0.${filesuffix}"
-    git add "${dirname}/${fileprefix}.[0-9]00.${filesuffix}"
-    git add "${dirname}/${fileprefix}.[0-9][0-9]00.${filesuffix}"
+    git add "${dirname}/${fileprefix}0${filesuffix}"
+    git add "${dirname}/${fileprefix}[0-9]00${filesuffix}"
+    git add "${dirname}/${fileprefix}[0-9][0-9]00${filesuffix}"
     git commit -m "Add ${filetype} files (index < 9000)"
 
     if [ "$speed" -eq 1 ]
@@ -62,11 +68,11 @@ if [[ -n "$dirname" && -n "$fileprefix" && -n "$filesuffix" ]]; then
         
       for i in {1..39}
       do
-        git add "${dirname}/${fileprefix}.${i}[0-9][0-9]00.${filesuffix}"
+        git add "${dirname}/${fileprefix}${i}[0-9][0-9]00${filesuffix}"
         git commit -m "Add ${filetype} files (index < ${i}9900)"
       done
       
-      git add "${dirname}/${fileprefix}.40[0-9][0-9]00.${filesuffix}"
+      git add "${dirname}/${fileprefix}40[0-9][0-9]00${filesuffix}"
       git commit -m "Add last ${filetype} file (400000)"
 
     elif [ "$speed" -eq 2 ]
@@ -74,11 +80,11 @@ if [[ -n "$dirname" && -n "$fileprefix" && -n "$filesuffix" ]]; then
 
       for i in {1..19}
       do
-        git add "${dirname}/${fileprefix}.${i}[0-9][0-9]00.${filesuffix}"
+        git add "${dirname}/${fileprefix}${i}[0-9][0-9]00${filesuffix}"
         git commit -m "Add ${filetype} files (index < ${i}9900)"
       done
       
-      git add "${dirname}/${fileprefix}.20[0-9][0-9]00.${filesuffix}"
+      git add "${dirname}/${fileprefix}20[0-9][0-9]00${filesuffix}"
       git commit -m "Add last ${filetype} file (200000)"
       
     elif [ "$speed" -eq 3 ]
@@ -86,14 +92,14 @@ if [[ -n "$dirname" && -n "$fileprefix" && -n "$filesuffix" ]]; then
 
       for i in {1..12}
       do
-        git add "${dirname}/${fileprefix}.${i}[0-9][0-9]00.${filesuffix}"
+        git add "${dirname}/${fileprefix}${i}[0-9][0-9]00${filesuffix}"
         git commit -m "Add ${filetype} files (index < ${i}9900)"
       done
       
-      git add "${dirname}/${fileprefix}.13[0-2][0-9]00.${filesuffix}"
+      git add "${dirname}/${fileprefix}13[0-2][0-9]00${filesuffix}"
       git commit -m "Add ${filetype} files (index < 132900)"
 
-      git add "${dirname}/${fileprefix}.13[0-9][0-9]00.${filesuffix}"
+      git add "${dirname}/${fileprefix}13[0-9][0-9]00${filesuffix}"
       git commit -m "Add last ${filetype} file (133000)"
       
     else
